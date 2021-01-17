d339680df4a6ae15e1ef0f618f0f9e46f5196e93
Annie Kong <kongannie97@gmail.com>
2021-01-17 16:27:11 -0500

    add exclamation mark next to result word, untrack .eslintcache

--- src/components/GeneratorBox/index.jsx
+++ src/components/GeneratorBox/index.jsx
@@ -18,7 +18,7 @@ function GeneratorBox (props) {
         <Paper elevation={3} className={classes.formPaper}>
             {type==="insult" && <Typography variant="h2" style={{paddingTop: 40}}> You </Typography>}
             <Container className={classes.wordContainer} id="wordContainer"> 
-                {result && <Typography variant="h1">{result}</Typography>}
+                {result && <Typography variant="h1">{result} !</Typography>}
             </Container>
             <Button onClick={generateWord}> Generate! </Button>
         </Paper>
